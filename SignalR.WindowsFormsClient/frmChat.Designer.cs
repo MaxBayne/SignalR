91349b3d894fafe252326a954f7b2e3017d034af
Mohamed Salah <maxbayne@gmail.com>
2023-06-20 14:31:42 +0300

    create windows forms client for signalR

---
+++ SignalR.WindowsFormsClient/frmChat.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace SignalR.WindowsFormsClient
+{
+    partial class FrmChat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.disconnectButton = new System.Windows.Forms.Button();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.messageTextBox = new System.Windows.Forms.TextBox();
+            this.messagesList = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.txt_sender = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // disconnectButton
+            // 
+            this.disconnectButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.disconnectButton.Enabled = false;
+            this.disconnectButton.Location = new System.Drawing.Point(648, 10);
+            this.disconnectButton.Name = "disconnectButton";
+            this.disconnectButton.Size = new System.Drawing.Size(75, 23);
+            this.disconnectButton.TabIndex = 13;
+            this.disconnectButton.Text = "Disconnect";
+            this.disconnectButton.UseVisualStyleBackColor = true;
+            this.disconnectButton.Click += new System.EventHandler(this.disconnectButton_Click);
+            // 
+            // sendButton
+            // 
+            this.sendButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.sendButton.Enabled = false;
+            this.sendButton.Location = new System.Drawing.Point(648, 285);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(75, 23);
+            this.sendButton.TabIndex = 12;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // messageTextBox
+            // 
+            this.messageTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.messageTextBox.Enabled = false;
+            this.messageTextBox.Location = new System.Drawing.Point(154, 287);
+            this.messageTextBox.Name = "messageTextBox";
+            this.messageTextBox.Size = new System.Drawing.Size(488, 20);
+            this.messageTextBox.TabIndex = 11;
+            this.messageTextBox.Enter += new System.EventHandler(this.messageTextBox_Enter);
+            // 
+            // messagesList
+            // 
+            this.messagesList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.messagesList.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.messagesList.FormattingEnabled = true;
+            this.messagesList.Location = new System.Drawing.Point(12, 38);
+            this.messagesList.Name = "messagesList";
+            this.messagesList.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.messagesList.Size = new System.Drawing.Size(711, 238);
+            this.messagesList.TabIndex = 10;
+            this.messagesList.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.messagesList_DrawItem);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Address:";
+            // 
+            // connectButton
+            // 
+            this.connectButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.connectButton.Location = new System.Drawing.Point(567, 10);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(75, 23);
+            this.connectButton.TabIndex = 8;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.addressTextBox.Location = new System.Drawing.Point(63, 12);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(498, 20);
+            this.addressTextBox.TabIndex = 7;
+            this.addressTextBox.Text = "https://localhost:7032/ChatHub\r\n";
+            this.addressTextBox.Enter += new System.EventHandler(this.addressTextBox_Enter);
+            // 
+            // txt_sender
+            // 
+            this.txt_sender.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_sender.Enabled = false;
+            this.txt_sender.Location = new System.Drawing.Point(12, 288);
+            this.txt_sender.Name = "txt_sender";
+            this.txt_sender.Size = new System.Drawing.Size(136, 20);
+            this.txt_sender.TabIndex = 14;
+            this.txt_sender.Text = "Desktop";
+            // 
+            // FrmChat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 319);
+            this.Controls.Add(this.txt_sender);
+            this.Controls.Add(this.disconnectButton);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.messageTextBox);
+            this.Controls.Add(this.messagesList);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.addressTextBox);
+            this.Name = "FrmChat";
+            this.Text = "Chat";
+            this.Load += new System.EventHandler(this.frmChat_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button disconnectButton;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.TextBox messageTextBox;
+        private System.Windows.Forms.ListBox messagesList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.TextBox txt_sender;
+    }
+}
+
---
+++ SignalR.WindowsFormsClient/frmChat.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace SignalR.WindowsFormsClient
+{
+    public partial class FrmChat : Form
+    {
+        private HubConnection _connection;
+
+        public FrmChat()
+        {
+            InitializeComponent();
+        }
+
+
+        private void frmChat_Load(object sender, EventArgs e)
+        {
+            addressTextBox.Focus();
+        }
+
+
+        #region Button
+
+        private async void connectButton_Click(object sender, EventArgs e)
+        {
+            UpdateState(connected: false);
+
+            _connection = new HubConnectionBuilder()
+                .WithUrl(addressTextBox.Text)
+                .Build();
+
+            _connection.On<string, string>("ReceiveMessage", (name, message) =>
+            {
+                Log(Color.Black, name + ": " + message);
+            });
+
+            Log(Color.Gray, "Starting connection...");
+            try
+            {
+                await _connection.StartAsync();
+            }
+            catch (Exception ex)
+            {
+                Log(Color.Red, ex.ToString());
+                return;
+            }
+
+            Log(Color.Gray, "Connection established.");
+
+            UpdateState(connected: true);
+
+            messageTextBox.Focus();
+        }
+
+        private async void disconnectButton_Click(object sender, EventArgs e)
+        {
+            Log(Color.Gray, "Stopping connection...");
+            try
+            {
+                await _connection.StopAsync();
+            }
+            catch (Exception ex)
+            {
+                Log(Color.Red, ex.ToString());
+            }
+
+            Log(Color.Gray, "Connection terminated.");
+
+            UpdateState(connected: false);
+        }
+
+        private async void sendButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                await _connection.InvokeAsync("SendMessage", txt_sender.Text, messageTextBox.Text);
+            }
+            catch (Exception ex)
+            {
+                Log(Color.Red, ex.ToString());
+            }
+        }
+        #endregion
+
+        #region TextBox
+        private void addressTextBox_Enter(object sender, EventArgs e)
+        {
+            AcceptButton = connectButton;
+        }
+
+        private void messageTextBox_Enter(object sender, EventArgs e)
+        {
+            AcceptButton = sendButton;
+        }
+
+        #endregion
+
+        #region List
+
+        private void messagesList_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            var message = (LogMessage)messagesList.Items[e.Index];
+            e.Graphics.DrawString(
+                message.Content,
+                messagesList.Font,
+                new SolidBrush(message.MessageColor),
+                e.Bounds);
+        }
+
+        #endregion
+
+
+
+
+
+        #region Helper
+
+        private void UpdateState(bool connected)
+        {
+            disconnectButton.Enabled = connected;
+            connectButton.Enabled = !connected;
+            addressTextBox.Enabled = !connected;
+
+            messageTextBox.Enabled = connected;
+            sendButton.Enabled = connected;
+        }
+
+        private void Log(Color color, string message)
+        {
+            Action callback = () =>
+            {
+                messagesList.Items.Add(new LogMessage(color, message));
+            };
+
+            Invoke(callback);
+        }
+
+        private class LogMessage
+        {
+            public Color MessageColor { get; }
+
+            public string Content { get; }
+
+            public LogMessage(Color messageColor, string content)
+            {
+                MessageColor = messageColor;
+                Content = content;
+            }
+        }
+
+        #endregion
+
+
+
+
+    }
+}
